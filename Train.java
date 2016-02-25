cb3020713796a67be83e2388d763c8784933c47c
kyle4765 <kg4765@gmail.com>
2016-02-25 11:00:37 -0500

    Create Train.java

---
+++ Train.java
@@ -0,0 +1,14 @@
+public class Train
+{
+  protected ArrayList<RailCar> cars;
+  
+  public Train()
+  {
+  }
+  public void addCar(String type, int x, int y)
+  {
+  }
+  public void addCar(int loc, String type, int x, int y)
+  {
+  }
+}
