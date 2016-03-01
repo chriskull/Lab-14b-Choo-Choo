88b214af5c894c682c6988b6b2bc710843a507b7
Christopher MacLean <chrismaclean220@gmail.com>
2016-03-01 09:27:41 -0500

    Update RailCar.java

--- RailCar.java
+++ RailCar.java
@@ -1,10 +1,5 @@
-// RailCar.java
 // This is the <RailCar> class that is the 
 // super class for other train classes.
-// This is one of the student starting files
-// of the Lab14b lab assignment.
-
-
 import java.awt.*;
 
 
@@ -21,27 +16,27 @@ public class RailCar
 		yPos = yP;
 	}
 
-	public void drawCar(Graphics g)
+	public void drawCar(Graphics g) //This draws the basic rail car
 	{
       drawContainer(g);
       drawRailWheels(g);
       drawLink(g);
 	}
    
-   private void drawContainer(Graphics g)
+   private void drawContainer(Graphics g) //This creates the body of the basic rail car
    {
    	g.setColor(carColor);
    	g.fillRect(xPos,yPos,150,100);
    }
    
-   private void drawRailWheels(Graphics g)
+   private void drawRailWheels(Graphics g) //This creates the wheels of the basic rail car
    {
    	g.setColor(Color.black);
 		g.fillOval(xPos+5,yPos+75,50,50);
 		g.fillOval(xPos+95,yPos+75,50,50);  
 	}
     
-   private void drawLink(Graphics g)
+   private void drawLink(Graphics g) //This creates the connector that will connect the rail cars
    {
       g.setColor(Color.black);
       g.fillRect(xPos-25,yPos+80,25,5);
